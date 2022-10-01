33f18bb669c155492facaaf1870153a6a8bdb40b
Maria Gio <alvarezgiomariaisabel@gmail.com>
2022-10-01 13:27:57 +0200

    Commit final

--- iMac.php
+++ iMac.php
@@ -1,28 +1,5 @@
 <?php
-class iMac{
-    public const marca="Apple";
-    public $software;
-
-    /**
-     * Get the value of software
-     */ 
-    public function getSoftware()
-    {
-        return $this->software;
-    }
-
-    /**
-     * Set the value of software
-     *
-     * @return  self
-     */ 
-    public function setSoftware($software)
-    {
-        $this->software = $software;
-
-        return $this;
-    }
-    public function getMarca(){
-        return $this->marca;
-    }
+interface iMac{
+    public const marca='Apple';
+    public const software='IOS';
 }
